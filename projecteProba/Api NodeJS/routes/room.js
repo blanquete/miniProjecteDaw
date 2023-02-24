82818261c439046380d381a1c6c4cd99ee0ce30d
loliverasdam <83054288+llucoliveras@users.noreply.github.com>
2023-02-24 16:18:21 +0100

    User teacher from room

--- projecteProba/Api NodeJS/routes/room.js
+++ projecteProba/Api NodeJS/routes/room.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const _ = require('lodash');
 const Room = require('../classes/Room');
+const User = require('../classes/User');
 const router = express();
 
 // GET ALL ROOMS BY USER (TEACHER)
@@ -19,7 +20,9 @@ router.get("/group/:idgroup", (req, res) => {
     Room.findAll({
         where: {
             group_idgroup: req.params.idgroup
-        }
+        },
+        include: User,
+        attributes: ["idroom", "name"]
     })
     .then(result => res.json(result))
     .catch(error => res.send(error).status(500))
